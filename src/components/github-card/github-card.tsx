e9e3f0caf26824a0210d66fd02963e9a7db12a55
Sulaiman Khalil <sulaiman.khalil@ibm.com>
2022-10-21 12:06:02 +0200

    eslint

--- src/components/github-card/github-card.tsx
+++ src/components/github-card/github-card.tsx
@@ -1,56 +1,52 @@
-import { Component, Host, h, Listen, State, Prop, Watch } from '@stencil/core';
+import { Component, Host, h, Listen, State, Prop, Watch } from "@stencil/core";
 // import { Api } from '../api';
 
 @Component({
-  tag: 'github-card',
-  styleUrl: 'github-card.css',
-  shadow: true,
+    tag: "github-card",
+    styleUrl: "github-card.css",
+    shadow: true,
 })
 export class GithubCard {
-  @Prop() data: any;
-  @Prop() user!: string;
-  @Watch('user')
-  async componentWillLoad() {
-    const res = await fetch(`https://api.github.com/users/${this.user}`);
-    const data = await res.json();
-    this.data = data;
+    @Prop() data: any;
+    @Prop() user!: string;
+    @Watch("user")
+    async componentWillLoad() {
+        const res = await fetch(`https://api.github.com/users/${this.user}`);
+        const data = await res.json();
+        this.data = data;
 
-    console.log(this.data);
-  }
-  render() {
-    return (
-      <div class="all-all">
-        {/* <h1>this input is :{this.user}</h1> */}
-
-        <div class="all">
-          <div class="myimage">
-            <img class="profil" src={this.data.avatar_url}></img>
-          </div>
-          <div class="name">
-            <h1> {this.data.name}</h1>
-          </div>
-          <div class="secound">
-            <ul class="info">
-              <li>
-                <p>Followers </p>
-                <span>{this.data.followers}</span>
-              </li>
-              <li>
-                <p>Following</p>
-                <span>{this.data.following} </span>
-              </li>
-              <li>
-                <p>Gists </p>
-                <span>{this.data.public_gists}</span>
-              </li>
-              <li>
-                <p>Repos </p>
-                <span>{this.data.public_repos}</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        console.log(this.data);
+    }
+    render() {
+        return (
+            <div class="all">
+                <div class="myimage">
+                    <img class="profil" src={this.data.avatar_url}></img>
+                </div>
+                <div class="name">
+                    <h1> {this.data.name}</h1>
+                </div>
+                <div class="secound">
+                    <ul class="info">
+                        <li>
+                            <p>Followers </p>
+                            <span>{this.data.followers}</span>
+                        </li>
+                        <li>
+                            <p>Following</p>
+                            <span>{this.data.following} </span>
+                        </li>
+                        <li>
+                            <p>Gists </p>
+                            <span>{this.data.public_gists}</span>
+                        </li>
+                        <li>
+                            <p>Repos </p>
+                            <span>{this.data.public_repos}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -1,50 +1,58 @@
-import { Component, h, Listen, State } from '@stencil/core';
+import { Component, h, Listen, State } from "@stencil/core";
+
 // import { Api } from '../api';
 
 @Component({
-  tag: 'search-form',
-  styleUrl: 'search-form.css',
-  shadow: true,
+    tag: "search-form",
+    styleUrl: "search-form.css",
+    shadow: true,
 })
 export class SearchForm {
-  @State() textInput!: HTMLInputElement;
-  @State() unsplash: undefined;
-  @State() dataItemes: any;
-  @State() data: any;
+    @State() textInput!: HTMLInputElement;
+    @State() unsplash: undefined;
+    @State() dataItemes: any;
+    @State() data: any;
 
-  @Listen('click', { capture: true })
-  handleSubmit(e: Event) {
-    e.preventDefault();
+    @Listen("click", { capture: true })
+    handleSubmit(e: Event) {
+        e.preventDefault();
+        if (this.textInput.value) {
+            fetch(
+                `https://api.unsplash.com/photos/random?client_id=nIN9of7odLV7T3z7JRmoAx4SJpKHJYOoPXZ4LwRAlJE`
+            )
+                .then((data) => data.json())
+                .then((res) => {
+                    this.unsplash = res.urls.regular;
+                    console.log("this.unspalsh :>> ", this.unsplash);
+                });
+        }
 
-    fetch(`https://api.unsplash.com/photos/random?client_id=zTlsfycuk6GJWIyc7rnOmFIwqYjXoBMyz2UQe7QEH8A`)
-      .then(data => data.json())
-      .then(res => {
-        this.unsplash = res.urls.regular;
-        console.log('this.unspalsh :>> ', this.unsplash);
-      });
+        console.log("button clicked");
+    }
 
-    console.log('button clicked');
-  }
+    render() {
+        return (
+            <div class="all-all">
+                <form>
+                    <input
+                        placeholder="Enter Github name"
+                        type="text"
+                        ref={(el) => (this.textInput = el as HTMLInputElement)}
+                    ></input>
+                    <button onSubmit={this.handleSubmit} type="submit">
+                        fetch&&search
+                    </button>
+                </form>
+                {this.unsplash && (
+                    <div>
+                        <img class="back" src={this.unsplash}></img>
+                        <github-card user={this.textInput.value}></github-card>
+                    </div>
+                )}
 
-  render() {
-    return (
-      <div class="all-all">
-        <form>
-          <input placeholder="Enter Github name" type="text" ref={el => (this.textInput = el as HTMLInputElement)}></input>
-          <button onSubmit={this.handleSubmit} type="submit">
-            fetch&&search
-          </button>
-        </form>
-        {this.unsplash && (
-          <div>
-            <img class="back" src={this.unsplash}></img>
-            <github-card user={this.textInput.value}></github-card>
-          </div>
-        )}
-
-        {/* <github-card user={this.textInput.value}></github-card> */}
-        {/* <github-card user={this.textInput.value}></github-card> */}
-        {/* {this.data && (
+                {/* <github-card user={this.textInput.value}></github-card> */}
+                {/* <github-card user={this.textInput.value}></github-card> */}
+                {/* {this.data && (
           <div class="all">
             <div class="myimage">
             
@@ -75,7 +83,7 @@ export class SearchForm {
             </div>
           </div>
         )} */}
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
