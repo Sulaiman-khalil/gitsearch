232ff7e5c98318363fe2752e2b320edd5b52878f
Sulaiman Khalil <sulaiman.khalil@ibm.com>
2022-10-21 16:36:58 +0200

    clean

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -49,40 +49,6 @@ export class SearchForm {
                         <github-card user={this.textInput.value}></github-card>
                     </div>
                 )}
-
-                {/* <github-card user={this.textInput.value}></github-card> */}
-                {/* <github-card user={this.textInput.value}></github-card> */}
-                {/* {this.data && (
-          <div class="all">
-            <div class="myimage">
-            
-              <img class="profil" src={this.data.avatar_url}></img>
-            </div>
-            <div class="name">
-              <h1> {this.data.name}</h1>
-            </div>
-            <div class="secound">
-              <ul class="info">
-                <li>
-                  <p>Followers </p>
-                  <span>{this.data.followers}</span>
-                </li>
-                <li>
-                  <p>Following</p>
-                  <span>{this.data.following} </span>
-                </li>
-                <li>
-                  <p>Gists </p>
-                  <span>{this.data.public_gists}</span>
-                </li>
-                <li>
-                  <p>Repos </p>
-                  <span>{this.data.public_repos}</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        )} */}
             </div>
         );
     }
